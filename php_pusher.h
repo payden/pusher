44ef4e8b09db26dd7e0926c10c66866f33bdeef6
Payden Sutherland <payden@paydensutherland.com>
2012-03-10 10:45:23 -0500

    Playing around with custom object storage and retrieval

--- php_pusher.h
+++ php_pusher.h
@@ -4,12 +4,23 @@
 #define PHP_PUSHER_VERSION NO_VERSION_YET
 #define PHP_PUSHER_EXTNAME "pusher"
 
+//PHP Function prototypes
 PHP_METHOD(pusher, __construct);
+PHP_METHOD(pusher, getVal);
 PHP_MINIT_FUNCTION(pusher);
+PHP_MSHUTDOWN_FUNCTION(pusher);
 
+//Typedefs
+
+typedef struct {
+	zend_object zo;
+	int *val;
+} pusher_object;
 
 static zend_class_entry *pusher_ce;
 void register_pusher_class(TSRMLS_D);
+static void pusher_objects_dtor(void *object, zend_object_handle handle TSRMLS_DC);
+
 extern zend_module_entry pusher_module_entry;
 
 #define phpext_pusher_ptr &pusher_module_entry
--- pusher.c
+++ pusher.c
@@ -6,34 +6,68 @@
 #include "php_pusher.h"
 
 
+zend_object_handlers pusher_object_handlers;
 extern zend_class_entry *pusher_ce;
 
 static zend_function_entry pusher_methods[] = {
-	PHP_ME(pusher, __construct, NULL, ZEND_ACC_PUBLIC)
+	PHP_ME(pusher, __construct, NULL, ZEND_ACC_PUBLIC | ZEND_ACC_CTOR)
+	PHP_ME(pusher, getVal, NULL, ZEND_ACC_PUBLIC)
 	{NULL, NULL, NULL}
 };
 
 PHP_METHOD(pusher, __construct) {
-	zval *pusher_settings;
-	ALLOC_INIT_ZVAL(pusher_settings);
-	array_init(pusher_settings);
-	zend_update_property(pusher_ce, getThis(), "settings", strlen("settings"), pusher_settings TSRMLS_CC);
-	RETURN_TRUE;
-}
+	zval *obj;
 
-PHP_METHOD(pusher, __destruct) {
-	zval *pusher_settings, *obj;
 	obj = getThis();
-	zend_read_property(Z_OBJCE_P(obj), obj, "settings", strlen("settings"), 1 TSRMLS_CC);
-	zval_dtor(pusher_settings);
+	pusher_object *pusher_obj = (pusher_object *)zend_object_store_get_object(obj TSRMLS_CC);
+	pusher_obj->val = (int *)emalloc(sizeof(int));
+	*(pusher_obj->val) = 10;
+
 	RETURN_TRUE;
 }
 
+PHP_METHOD(pusher, getVal) {
+	zval *obj;
+	obj = getThis();
+	pusher_object *pusher_obj = (pusher_object *)zend_object_store_get_object(obj TSRMLS_CC);
+	RETURN_LONG(*(pusher_obj->val));
+}
+
+
+static void pusher_free_storage(void *object TSRMLS_DC) {
+	pusher_object *intern = (pusher_object *)object;
+	int *var = intern->val;
+	efree(var);
+	zend_hash_destroy(intern->zo.properties);
+	FREE_HASHTABLE(intern->zo.properties);
+	efree(intern);
+}
+
+
+zend_object_value pusher_object_create(zend_class_entry *class_type TSRMLS_DC) {
+	zend_object_value retval;
+	zval *tmp;
+	pusher_object *intern;
+
+	intern = (pusher_object *) emalloc(sizeof(pusher_object));
+	memset(intern, 0, sizeof(pusher_object));
+	intern->zo.ce = class_type;
+	ALLOC_HASHTABLE(intern->zo.properties);
+	zend_hash_init(intern->zo.properties, 0, NULL, ZVAL_PTR_DTOR, 0);
+	zend_hash_copy(intern->zo.properties, &class_type->default_properties, (copy_ctor_func_t) zval_add_ref, (void *) &tmp, sizeof(zval *));
+
+	retval.handle = zend_objects_store_put(intern, NULL, pusher_free_storage, NULL TSRMLS_CC);
+	retval.handlers = (zend_object_handlers *) &pusher_object_handlers;
+	return retval;
+}
 
 void register_pusher_class(TSRMLS_D) {
 	zend_class_entry ce;
 	INIT_CLASS_ENTRY(ce, "Pusher", pusher_methods);
+	ce.create_object = pusher_object_create;
+	memcpy(&pusher_object_handlers, zend_get_std_object_handlers(), sizeof(zend_object_handlers));
+	pusher_object_handlers.clone_obj = NULL;
 	pusher_ce = zend_register_internal_class(&ce TSRMLS_CC);
-	zend_declare_property_null(pusher_ce, "settings", strlen("settings"), ZEND_ACC_PROTECTED TSRMLS_CC);
 }
+
 
