0cbe1d3e857f28fb13cb45e7a4042e4404fafb0d
Payden Sutherland <payden@paydensutherland.com>
2012-03-11 13:04:39 -0400

    Just moving stuff around.

--- pusher.c
+++ pusher.c
@@ -11,12 +11,68 @@
 
 extern zend_class_entry *pusher_ce;
 
+
+
 static zend_function_entry pusher_methods[] = {
 	PHP_ME(pusher, __construct, NULL, ZEND_ACC_PUBLIC | ZEND_ACC_CTOR)
 	PHP_ME(pusher, getKey, NULL, ZEND_ACC_PUBLIC)
 	PHP_ME(pusher, trigger, NULL, ZEND_ACC_PUBLIC)
 	{NULL, NULL, NULL}
 };
+
+static void pusher_free_storage(void *object TSRMLS_DC) {
+        pusher_object *intern = (pusher_object *)object;
+        char *key = (char *)intern->key;
+        char *secret = (char *)intern->secret;
+        if(key) { efree(key); }
+        if(secret) { efree(secret); }
+        zend_hash_destroy(intern->zo.properties);
+        FREE_HASHTABLE(intern->zo.properties);
+        efree(intern);
+}
+
+static size_t curl_write_data(void *buffer, size_t size, size_t nmemb, void *userp) {
+        return size * nmemb;
+}
+
+static char *md5_hash(const char *str) {
+        md5_state_t *state;
+        md5_byte_t digest[16];
+        char *md5_str, *md5_str_ptr;
+        int i, x;
+        state = (md5_state_t *)emalloc(sizeof(md5_state_t));
+        md5_str = md5_str_ptr = (char *)emalloc(33); //always 32 bytes long + '\0'
+        memset(md5_str, '\0', 33);
+        md5_init(state);
+        md5_append(state, (const md5_byte_t *)str, strlen(str));
+        md5_finish(state, digest);
+        for(i=0,x=0;i<16;i++,x+=2) {
+                sprintf(md5_str+x, "%02x", digest[i]);
+        }
+        efree(state);
+        return md5_str_ptr;
+}
+
+static zend_object_value pusher_object_create(zend_class_entry *class_type TSRMLS_DC) {
+        zend_object_value retval;
+        pusher_object *intern;
+        intern = (pusher_object *) emalloc(sizeof(pusher_object));
+        memset(intern, 0, sizeof(pusher_object));
+        intern->zo.ce = class_type;
+        ALLOC_HASHTABLE(intern->zo.properties);
+        zend_hash_init(intern->zo.properties, 0, NULL, ZVAL_PTR_DTOR, 0);
+#if ZEND_MODULE_API_NO >= 20100525
+        object_properties_init(&(intern->zo), class_type);
+#else
+        zval *tmp;
+        zend_hash_copy(intern->zo.properties, &class_type->default_properties, (copy_ctor_func_t) zval_add_ref, (void *) &tmp, sizeof(zval *));
+#endif
+
+        retval.handle = zend_objects_store_put(intern, NULL, pusher_free_storage, NULL TSRMLS_CC);
+        retval.handlers = (zend_object_handlers *) &pusher_object_handlers;
+        return retval;
+}
+
 
 PHP_METHOD(pusher, __construct) {
 	zval *this;
@@ -130,61 +186,6 @@ PHP_METHOD(pusher, getKey) {
 	RETURN_STRING(pusher_obj->key, 1);
 }
 
-static size_t curl_write_data(void *buffer, size_t size, size_t nmemb, void *userp) {
-	return size * nmemb;
-}
-
-static char *md5_hash(const char *str) {
-	md5_state_t *state;
-	md5_byte_t digest[16];
-	char *md5_str, *md5_str_ptr;
-	int i, x;
-	state = (md5_state_t *)emalloc(sizeof(md5_state_t));
-	md5_str = md5_str_ptr = (char *)emalloc(33); //always 32 bytes long + '\0'
-	memset(md5_str, '\0', 33);
-	md5_init(state);
-	md5_append(state, (const md5_byte_t *)str, strlen(str));
-	md5_finish(state, digest);
-	for(i=0,x=0;i<16;i++,x+=2) {
-		sprintf(md5_str+x, "%02x", digest[i]);
-	}
-	efree(state);
-	return md5_str_ptr;
-}
-
-
-
-static void pusher_free_storage(void *object TSRMLS_DC) {
-	pusher_object *intern = (pusher_object *)object;
-	char *key = (char *)intern->key;
-	char *secret = (char *)intern->secret;
-	if(key) { efree(key); }
-	if(secret) { efree(secret); }
-	zend_hash_destroy(intern->zo.properties);
-	FREE_HASHTABLE(intern->zo.properties);
-	efree(intern);
-}
-
-
-zend_object_value pusher_object_create(zend_class_entry *class_type TSRMLS_DC) {
-	zend_object_value retval;
-	pusher_object *intern;
-	intern = (pusher_object *) emalloc(sizeof(pusher_object));
-	memset(intern, 0, sizeof(pusher_object));
-	intern->zo.ce = class_type;
-	ALLOC_HASHTABLE(intern->zo.properties);
-	zend_hash_init(intern->zo.properties, 0, NULL, ZVAL_PTR_DTOR, 0);
-#if ZEND_MODULE_API_NO >= 20100525
-	object_properties_init(&(intern->zo), class_type);
-#else
-	zval *tmp;
-	zend_hash_copy(intern->zo.properties, &class_type->default_properties, (copy_ctor_func_t) zval_add_ref, (void *) &tmp, sizeof(zval *));
-#endif
-
-	retval.handle = zend_objects_store_put(intern, NULL, pusher_free_storage, NULL TSRMLS_CC);
-	retval.handlers = (zend_object_handlers *) &pusher_object_handlers;
-	return retval;
-}
 
 void register_pusher_class(TSRMLS_D) {
 	zend_class_entry ce;
